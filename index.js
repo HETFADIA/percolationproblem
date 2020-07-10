6efd1736a1d759333d4e0752d3ec0592eda98edd
Het <62541263+HETFADIA@users.noreply.github.com>
2020-07-10 16:08:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var a=1;
+console.log(a)
+let dictionary={name: "het", age:20}
+dictionary.name="Het Fadia"
+dictionary["name"]="het fadia"
+dictionary["hisname"]="het fadia"
+console.log(dictionary)
+// console.log(dictionary["time"])
