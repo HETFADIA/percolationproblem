6ee2821d0bb4502eb8762507d5244b74a6ad5088
Het <62541263+HETFADIA@users.noreply.github.com>
2020-07-12 14:43:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ dictionary["name"]="het fadia"
 dictionary["hisname"]="het fadia"
 console.log(dictionary)
 // console.log(dictionary["time"])
+
+
+
+
+// node "file name".js
+
+
+// document.getElementsByClassName("area")
+
+// document.getElementsByClassName("area")[0]
+
+// document.getElementsByClassName("area")[0].innerText
+
+
+
+// document.getElementsByClassName("area")[0].style.color="red";
+
+// document.querySelector("body").style.backgroundColor="black";
+
+
+// document.write("hello world");
